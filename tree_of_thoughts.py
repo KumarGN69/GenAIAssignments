4185a4d6a4e8b2bee13e75a64d9931f293ad20a6
kumarGN69 <kumargn1969@otmail.com>
2025-05-03 12:53:39 -0500

    JSON formatted ouput for tree of thoughts

--- tree_of_thoughts.py
+++ tree_of_thoughts.py
@@ -34,11 +34,12 @@ ideas = client.generate(
         """
     
     ),
+    format=IdeasList.model_json_schema()
 )
 #parse the llm output into a structured list using pydantic classes
 ideas_list = IdeasList.model_validate_json(ideas.response)
 
-#Brain storm and write the ideas to a markdown file
+#Brainstorm and write the ideas to a markdown file
 with open("TreeOfThoughts.md","w",encoding="utf-8") as f:
     f.writelines(f"# Topic: {thought}"+"\n")
     for idea in ideas_list.solutions:       
